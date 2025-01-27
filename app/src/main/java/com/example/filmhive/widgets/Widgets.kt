61737fd43178d1cba572f17e7ef96d8f8321af23
Mohammed Yusuf <mohammedyusufmujawar92@gmail.com>
2025-01-27 01:26:26 -0600

    added some more information for the movie and also added a drop down and up arrow which expands this extra information. Used buildAnnotatedString in Text composable to make the string more customizable.

--- app/src/main/java/com/example/filmhive/widgets/Widgets.kt
+++ app/src/main/java/com/example/filmhive/widgets/Widgets.kt
@@ -1,5 +1,7 @@
 package com.example.filmhive.widgets
 
+import android.R
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,18 +12,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
@@ -33,11 +49,12 @@ import com.example.filmhive.model.getMovie
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovie()[0], onItemClick: (String) -> Unit = {}) {
+    var expanded = remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable { onItemClick(movie.id) },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = CardDefaults.cardElevation(6.dp)
@@ -70,6 +87,45 @@ fun MovieRow(movie: Movie = getMovie()[0], onItemClick: (String) -> Unit = {}) {
                     style = MaterialTheme.typography.labelMedium
                 )
                 Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.labelMedium)
+                AnimatedVisibility(visible = expanded.value) {
+                    Column {
+                        if (expanded.value) {
+                            Text(buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp
+                                    )
+                                ) {
+                                    append("Plot: ")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp,
+                                        fontWeight = FontWeight.Light
+                                    )
+                                ) {
+                                    append(movie.plot)
+                                }
+                            }, modifier = Modifier.padding(6.dp))
+                            HorizontalDivider(modifier = Modifier.padding(3.dp))
+                            Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelMedium)
+                            Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.labelMedium)
+                            Text(text = "Ratings: ${movie.rating}", style = MaterialTheme.typography.labelMedium)
+                        }
+                    }
+                }
+                Icon(
+                    imageVector = if (!expanded.value) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowUp,
+                    contentDescription = "Keyboard Arrow Down",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded.value = !expanded.value
+                        },
+                    tint = Color.DarkGray
+                )
             }
         }
     }
